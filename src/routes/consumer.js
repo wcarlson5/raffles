642b72b98c87bb9d7aee243120fb9274dcd48735
Aydrian J. Howard <aydrian@gmail.com>
2016-07-29 09:39:28 -0400

    basic push of raffle count via web socket

--- src/routes/consumer.js
+++ src/routes/consumer.js
@@ -3,17 +3,16 @@
 var Q = require('q')
   , router = require('express').Router() // eslint-disable-line new-cap
   , RelayMessage = require('../models/relayMessage')
+  , Raffle = require('../models/raffle')
   , SparkPost = require('sparkpost')
   , client = new SparkPost()
   , logger = require('../lib/logger');
-
-module.exports = router;
 
 function getTemplate(raffle) {
   var deferred = Q.defer()
     , templateId = 'raffle-' + raffle;
 
-  client.templates.find({ id: templateId }, function(err) {
+  client.templates.find({id: templateId}, function(err) {
     templateId = err ? 'raffle-default' : templateId;
     deferred.resolve(templateId);
   });
@@ -29,7 +28,7 @@ function sendConfirmationEmail(data) {
         template_id: data.templateId
       },
       substitution_data: data,
-      recipients: [{ address: { email: data.entryEmail } }]
+      recipients: [{address: {email: data.entryEmail}}]
     }
   }, function(err) {
     if (err) {
@@ -39,36 +38,48 @@ function sendConfirmationEmail(data) {
   });
 }
 
-function processRelayMessage(relayEvent) {
-  var data = {
-    entryEmail: relayEvent.msg_from,
-    raffle: relayEvent.rcpt_to.split('@')[0]
-  };
+module.exports = function(io) {
 
-  getTemplate(data.raffle)
-    .then(function(templateId) {
-      logger.info('Using Template: ' + templateId);
-      data.templateId = templateId;
-      return data;
-    })
-    .then(sendConfirmationEmail);
-}
+  function processRelayMessage(relayEvent) {
+    var data = {
+      entryEmail: relayEvent.msg_from,
+      raffle: relayEvent.rcpt_to.split('@')[0]
+    };
 
-router.post('/', function(req, res) {
-  var batch = req.body;
-  RelayMessage.createMany(batch)
-    .then(function() {
-      res.sendStatus(200);
-    })
-    .then(function() {
-      var i;
+    getTemplate(data.raffle)
+      .then(function(templateId) {
+        logger.info('Using Template: ' + templateId);
+        data.templateId = templateId;
+        return data;
+      })
+      .then(sendConfirmationEmail)
+      .then(function() {
+        Raffle.getRaffle(null, null, data.raffle).then(function(raffle) {
+          io.to(data.raffle).emit('count', {count: raffle.num_entries});
+        });
+      });
+  }
 
-      for(i = 0; i < batch.length; i++) {
-        processRelayMessage(batch[i].msys.relay_message);
-      }
-    })
-    .fail(function(err) {
-      logger.error('Oh no, something went wrong!');
-      logger.error(err);
-    });
-});
+  router.post('/', function(req, res) {
+    var batch = req.body;
+    console.log(batch);
+    RelayMessage.createMany(batch)
+      .then(function() {
+        res.sendStatus(200);
+      })
+      .then(function() {
+        var i;
+
+        for (i = 0; i < batch.length; i++) {
+          processRelayMessage(batch[i].msys.relay_message);
+        }
+      })
+      .fail(function(err) {
+        logger.error('Oh no, something went wrong!');
+        logger.error(err);
+        res.sendStatus(500);
+      });
+  });
+
+  return router;
+};
